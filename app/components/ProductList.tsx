a55051b0995834402663717787a018668e179d3e
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-10-06 08:11:04 -0500

    Modify homepage

--- app/components/ProductList.tsx
+++ app/components/ProductList.tsx
@@ -21,22 +21,22 @@ export default function ProductList() {
   }, []);
 
   return (
-    <div className="bg-white">
+    <div>
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
 
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+            <a key={product.id} href={`/products/${product.handle}`} className="group">
+              <div className="h-64 w-full overflow-hidden rounded-lg xl:aspect-h-8 xl:aspect-w-7">
                 <img
                   // alt={product.imageAlt}
-                  // src={product.imageSrc}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
+                  src={product.images?.[0]?.url}
+                  className="h-full w-full object-contain object-center group-hover:opacity-75"
                 />
               </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.title}</h3>
-              {/* <p className="mt-1 text-lg font-medium text-gray-900">{product.price}</p> */}
+              <h3 className="mt-4 text-sm text-gray-400">{product.title}</h3>
+              <p className="mt-1 text-lg font-medium text-gray-900">${product.variants?.[0]?.prices?.[0]?.amount / 100}</p>
             </a>
           ))}
         </div>
---
+++ app/layout/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="flex justify-center items-center text-2xl font-bold my-2">
+            <h1>Techno-Objectivist Store</h1>
+        </header>
+    )
+}
+
+export default Header;
