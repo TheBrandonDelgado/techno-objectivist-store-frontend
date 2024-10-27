71b8b4182054d025633c2c6e12d12dcd4248a542
Brandon Delgado <TheBrandonDelgado@gmail.com>
2024-10-27 08:01:36 -0500

    Update Cart.tsx

--- app/components/cart/Cart.tsx
+++ app/components/cart/Cart.tsx
@@ -1,20 +1,24 @@
-import { useCart } from "medusa-react"
+import { useCreateCart } from "medusa-react"
 
 const Cart = () => {
-  const { cart, createCart } = useCart()
-
-  const handleCreateCart = () => {
-    createCart.mutate(
-      {}, // create an empty cart
-      {
-        onSuccess: ({ cart }) => {
-          localStorage.setItem("cart_id", cart.id)
-        },
-      }
-    )
+  const createCart = useCreateCart()
+  const handleClick = () => {
+    createCart.mutate({}) // create an empty cart
   }
-  
-  // ...
+
+  return (
+    <div>
+      {createCart.isLoading && <div>Loading...</div>}
+      {!createCart.data?.cart && (
+        <button onClick={handleClick}>
+          Create cart
+        </button>
+      )}
+      {createCart.data?.cart?.id && (
+        <div>Cart ID: {createCart.data?.cart.id}</div>
+      )}
+    </div>
+  )
 }
 
 export default Cart
